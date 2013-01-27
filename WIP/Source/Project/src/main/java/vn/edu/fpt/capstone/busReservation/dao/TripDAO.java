3880e39952d5fb8f4f44383a4fcd70e2873ec1a7
tramntb60156 <tramntb60156@fpt.edu.vn>
2013-01-27 04:11:52 +0000

    RouteDAO and TripDAO

---
+++ WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/dao/TripDAO.java
@@ -0,0 +1,12 @@
+package vn.edu.fpt.capstone.busReservation.dao;
+
+import vn.edu.fpt.capstone.busReservation.dao.bean.TripBean;
+
+public class TripDAO extends GenericDAO<Integer, TripBean>{
+
+	public TripDAO(Class<TripBean> clazz) {
+		super(clazz);
+		// TODO Auto-generated constructor stub
+	}
+
+}
