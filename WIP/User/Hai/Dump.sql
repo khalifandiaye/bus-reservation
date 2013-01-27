d45f2ee5f79f0f21edcbb18f13a6e24ec0d5973a
hainl60335 <hainl60335@fpt.edu.vn>
2013-01-27 07:18:46 +0000

    update database

--- WIP/User/Hai/Dump.sql
+++ WIP/User/Hai/Dump.sql
@@ -59,12 +59,12 @@ CREATE TABLE `bus_status` (
   `from_date` datetime NOT NULL,
   `to_date` datetime NOT NULL,
   `status` varchar(10) NOT NULL,
-  `scheduler` int(11) NOT NULL,
+  `scheduler_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `bus_status_unique_key` (`bus_id`,`from_date`),
   KEY `bus_status_bus_id_idx` (`bus_id`),
-  KEY `bus_status_schedule_idx` (`scheduler`),
-  CONSTRAINT `bus_status_schedule` FOREIGN KEY (`scheduler`) REFERENCES `employee` (`id`),
+  KEY `bus_status_schedule_idx` (`scheduler_id`),
+  CONSTRAINT `bus_status_schedule` FOREIGN KEY (`scheduler_id`) REFERENCES `employee` (`id`),
   CONSTRAINT `bus_status_bus_id` FOREIGN KEY (`bus_id`) REFERENCES `bus` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -368,13 +368,13 @@ DROP TABLE IF EXISTS `tariff`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tariff` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `segment_in_route_id` int(11) NOT NULL,
+  `route_details_id` int(11) NOT NULL,
   `valid_from` datetime NOT NULL,
   `fare` double NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `segment_in_route_id` (`segment_in_route_id`,`valid_from`),
-  KEY `tariff_segment_in_route_id_idx` (`segment_in_route_id`),
-  CONSTRAINT `tariff_segment_in_route_id` FOREIGN KEY (`segment_in_route_id`) REFERENCES `route_details` (`id`)
+  UNIQUE KEY `segment_in_route_id` (`route_details_id`,`valid_from`),
+  KEY `tariff_segment_in_route_id_idx` (`route_details_id`),
+  CONSTRAINT `tariff_segment_in_route_id` FOREIGN KEY (`route_details_id`) REFERENCES `route_details` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -457,4 +457,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-01-27 12:13:19
+-- Dump completed on 2013-01-27 14:13:39
