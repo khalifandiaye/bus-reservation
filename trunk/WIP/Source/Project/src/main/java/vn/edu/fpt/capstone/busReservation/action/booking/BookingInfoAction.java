5988547606fff3cf2d1e25e7b0f7ecdff09672a8
hainl60335@fpt.edu.vn <hainl60335@fpt.edu.vn@2dff193b-118c-dc03-1232-ddc764ddf2ce>
2013-02-20 04:22:40 +0000

    load pay01010 part in booking-info.jsp (drop-down list only)

--- trunk/WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/action/booking/BookingInfoAction.java
+++ trunk/WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/action/booking/BookingInfoAction.java
@@ -2,41 +2,76 @@ package vn.edu.fpt.capstone.busReservation.action.booking;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.struts2.interceptor.SessionAware;
-
+import vn.edu.fpt.capstone.busReservation.action.BaseAction;
+import vn.edu.fpt.capstone.busReservation.dao.bean.PaymentMethodBean;
+import vn.edu.fpt.capstone.busReservation.dao.bean.TripBean;
+import vn.edu.fpt.capstone.busReservation.displayModel.ReservationInfo;
 import vn.edu.fpt.capstone.busReservation.displayModel.SeatInfo;
+import vn.edu.fpt.capstone.busReservation.logic.PaymentLogic;
+import vn.edu.fpt.capstone.busReservation.logic.ReservationLogic;
 
-import com.opensymphony.xwork2.ActionSupport;
+public class BookingInfoAction extends BaseAction {
 
-public class BookingInfoAction extends ActionSupport implements SessionAware{
-	
-	Map<String,Object> session = null;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    // ==========================Logic Object==========================
+    private PaymentLogic paymentLogic;
+    @SuppressWarnings("unused")
+    private ReservationLogic reservationLogic;
+
+    /**
+     * @param paymentLogic
+     *            the paymentLogic to set
+     */
+    public void setPaymentLogic(PaymentLogic paymentLogic) {
+        this.paymentLogic = paymentLogic;
+    }
+
+    /**
+     * @param reservationLogic
+     *            the reservationLogic to set
+     */
+    public void setReservationLogic(ReservationLogic reservationLogic) {
+        this.reservationLogic = reservationLogic;
+    }
 	private String selectedSeat;
-	private String[] seats; 
 	private List<SeatInfo> listSeats = new ArrayList<SeatInfo>();
+    private List<PaymentMethodBean> paymentMethods;
+    private ReservationInfo reservationInfo;
 	
 	public List<SeatInfo> getListSeats() {
 		return listSeats;
 	}
+
+    /**
+     * @return the paymentMethods
+     */
+    public List<PaymentMethodBean> getPaymentMethods() {
+        return paymentMethods;
+    }
+
+    /**
+     * @return the reservationInfo
+     */
+    public ReservationInfo getReservationInfo() {
+        return reservationInfo;
+    }
 
 	public void setSelectedSeat(String selectedSeat) {
 		this.selectedSeat = selectedSeat;
 	}
 
 	public String execute(){
+	    String[] seats;
 		session.put("selectedSeats", selectedSeat);
 		seats = selectedSeat.split(";");
 		for(int i= 0; i < seats.length; i++){
 			listSeats.add(new SeatInfo(seats[i], "2"));
 		}
+        paymentMethods = paymentLogic.getPaymentMethods();
 		return SUCCESS;
-	}
-
-	@Override
-	public void setSession(Map<String, Object> session) {
-		this.session = session;
-		
 	}
 }
