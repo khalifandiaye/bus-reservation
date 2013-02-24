65eb65bfbcbf92f40b13556de24ce1cafbbac840
hainl60335 <hainl60335@fpt.edu.vn>
2013-02-24 16:30:27 +0000

    fix deFormatDate() timeZone

--- WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/FormatUtils.java
+++ WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/FormatUtils.java
@@ -27,6 +27,7 @@ public class FormatUtils {
 	}
     public static Date deFormatDate(String date, String pattern, Locale locale, TimeZone timeZone) throws ParseException {
         DateFormat formatter = new SimpleDateFormat(pattern, locale);
+        formatter.setTimeZone(timeZone);
         return formatter.parse(date);
     }
 	
