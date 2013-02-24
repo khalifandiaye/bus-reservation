f6479e09a46e42a51526836699d1f299c244bb1b
hainl60335@fpt.edu.vn <hainl60335@fpt.edu.vn@2dff193b-118c-dc03-1232-ddc764ddf2ce>
2013-02-24 16:53:33 +0000

    fix deFormatDate() timeZone

--- trunk/WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/FormatUtils.java
+++ trunk/WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/FormatUtils.java
@@ -17,50 +17,61 @@ import java.util.TimeZone;
 
 /**
  * @author Yoshimi
- *
+ * 
  */
 public class FormatUtils {
-	public static String formatDate(Date date, String pattern, Locale locale, TimeZone timeZone) {
-		DateFormat formatter = new SimpleDateFormat(pattern, locale);
-		formatter.setTimeZone(timeZone);
-		return formatter.format(date);
-	}
-    public static Date deFormatDate(String date, String pattern, Locale locale, TimeZone timeZone) throws ParseException {
+    public static String formatDate(Date date, String pattern, Locale locale,
+            TimeZone timeZone) {
         DateFormat formatter = new SimpleDateFormat(pattern, locale);
         formatter.setTimeZone(timeZone);
-        return formatter.parse(date);
+        return formatter.format(date);
     }
-	
-	public static String formatCurrency(BigDecimal amount, int fractionDigits, String currency, Locale locale) {
-		NumberFormat format = NumberFormat.getCurrencyInstance(locale);
-		format.setCurrency(Currency.getInstance(currency));
-		format.setMaximumFractionDigits(fractionDigits);
-		format.setMinimumFractionDigits(fractionDigits);
-		return format.format(amount.doubleValue());
-	}
-	
-	public static String formatNumber(BigDecimal amount, int fractionDigits, Locale locale) {
-		NumberFormat format = NumberFormat.getInstance(locale);
-		format.setMaximumFractionDigits(fractionDigits);
-		format.setMinimumFractionDigits(fractionDigits);
-		format.setRoundingMode(RoundingMode.CEILING);
-		return format.format(amount.doubleValue());
-	}
-    
-    public static String formatNumber(double amount, int fractionDigits, Locale locale) {
+
+    public static Date deFormatDate(String date, String pattern, Locale locale,
+            TimeZone timeZone) throws ParseException {
+        DateFormat formatter = new SimpleDateFormat(pattern, locale);
+        Date result = formatter.parse(date);
+        long milisec = result.getTime();
+        result = new Date(milisec + TimeZone.getDefault().getOffset(milisec)
+                - timeZone.getOffset(milisec));
+        return result;
+    }
+
+    public static String formatCurrency(BigDecimal amount, int fractionDigits,
+            String currency, Locale locale) {
+        NumberFormat format = NumberFormat.getCurrencyInstance(locale);
+        format.setCurrency(Currency.getInstance(currency));
+        format.setMaximumFractionDigits(fractionDigits);
+        format.setMinimumFractionDigits(fractionDigits);
+        return format.format(amount.doubleValue());
+    }
+
+    public static String formatNumber(BigDecimal amount, int fractionDigits,
+            Locale locale) {
+        NumberFormat format = NumberFormat.getInstance(locale);
+        format.setMaximumFractionDigits(fractionDigits);
+        format.setMinimumFractionDigits(fractionDigits);
+        format.setRoundingMode(RoundingMode.CEILING);
+        return format.format(amount.doubleValue());
+    }
+
+    public static String formatNumber(double amount, int fractionDigits,
+            Locale locale) {
         NumberFormat format = NumberFormat.getInstance(locale);
         format.setMaximumFractionDigits(fractionDigits);
         format.setMinimumFractionDigits(fractionDigits);
         format.setRoundingMode(RoundingMode.CEILING);
         return format.format(amount);
     }
-	
-	public static BigDecimal deformatNumber(String string, Locale locale) throws ParseException {
-	    NumberFormat format = NumberFormat.getInstance(locale);
-	    return BigDecimal.valueOf(format.parse(string).doubleValue());
-	}
-	
-	public static void main(String[] args) {
-	    System.out.println(Arrays.toString(TimeZone.getAvailableIDs(TimeZone.getTimeZone("Asia/Ho_Chi_Minh").getRawOffset())));
-	}
+
+    public static BigDecimal deformatNumber(String string, Locale locale)
+            throws ParseException {
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return BigDecimal.valueOf(format.parse(string).doubleValue());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(TimeZone.getAvailableIDs(TimeZone
+                .getTimeZone("Asia/Ho_Chi_Minh").getRawOffset())));
+    }
 }
