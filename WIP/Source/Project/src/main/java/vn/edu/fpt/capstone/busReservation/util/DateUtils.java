09306c5b68d83489bc114e8951177d2113e32289
tramntb60156 <tramntb60156@fpt.edu.vn>
2013-02-28 16:14:49 +0000

    remove redundancy importing

--- WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/DateUtils.java
+++ WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/DateUtils.java
@@ -3,7 +3,6 @@ package vn.edu.fpt.capstone.busReservation.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
