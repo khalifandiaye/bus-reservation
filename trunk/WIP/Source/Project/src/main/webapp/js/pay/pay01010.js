08ad36f19423ad28a53bed19262f050259ec049b
hainl60335@fpt.edu.vn <hainl60335@fpt.edu.vn@2dff193b-118c-dc03-1232-ddc764ddf2ce>
2013-02-28 03:51:12 +0000

    calculate fee on seat change

--- trunk/WIP/Source/Project/src/main/webapp/js/pay/pay01010.js
+++ trunk/WIP/Source/Project/src/main/webapp/js/pay/pay01010.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
-    $('select').bind('change.fill_info',function(e) {
-        var evt = e ? e : event;
-        var target;
-        target = evt.target ? evt.target
-                : evt.source;
-//        if (console && console.log) {
-//            console.log("Value:", $(target)
-//                    .val());
-//        }
-        $.ajax({
-            type : "GET",
-            url : "calculateFee.html",
-            data : {
-                paymentMethodId : $(target).val()
-            },
-            success : function(data) {
-                $("#transactionFee").html(
-                        data.reservationInfo.transactionFee
-                        + " VND = $"
-                        + data.reservationInfo.transactionFeeInUSD);
-                $("#totalAmount").html(
-                        data.reservationInfo.totalAmount
-                        + " VND = $"
-                        + data.reservationInfo.totalAmountInUSD);
-            }
-        });
-    });
+    var calculateFee = function(e) {
+//      var evt = e ? e : event;
+//      var target;
+//      target = evt.target ? evt.target
+//              : evt.source;
+//      if (console && console.log) {
+//          console.log("Value:", $(target)
+//                  .val());
+//      }
+          $.ajax({
+              type : "GET",
+              url : $('#contextPath') + "/pay/calculateFee.html",
+              data : {
+                  paymentMethodId : $('#selectPaymentMethod').val(),
+                  selectedSeat : $.cookie('selectedSeat')
+              },
+              success : function(data) {
+                  $("#transactionFee").html(
+                          data.reservationInfo.transactionFee
+                          + " VND = $"
+                          + data.reservationInfo.transactionFeeInUSD);
+                  $("#totalAmount").html(
+                          data.reservationInfo.totalAmount
+                          + " VND = $"
+                          + data.reservationInfo.totalAmountInUSD);
+              }
+          });
+    };
+    $('#selectPaymentMethod').bind('change.fill_info', calculateFee);
+    $('.listCheckedSeats').on('click.fill_info', 'button', calculateFee);
     $('input#pay01020').bind('click.submit', function(e) {
         $('form').prop("action", $('#contextPath').val() + '/pay/pay01020.html');
         $('form').submit();
