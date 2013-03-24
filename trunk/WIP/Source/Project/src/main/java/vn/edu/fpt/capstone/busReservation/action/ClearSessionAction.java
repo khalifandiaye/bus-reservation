9efeb451ac4651d4a667316d90f4ea6b31ebec34
hainl60335@fpt.edu.vn <hainl60335@fpt.edu.vn@2dff193b-118c-dc03-1232-ddc764ddf2ce>
2013-03-24 17:52:55 +0000

    exception interceptor fix pay

--- trunk/WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/action/ClearSessionAction.java
+++ trunk/WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/action/ClearSessionAction.java
@@ -6,10 +6,9 @@ package vn.edu.fpt.capstone.busReservation.action;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 
-
 /**
  * @author Yoshimi
- *
+ * 
  */
 public class ClearSessionAction extends BaseAction {
 
@@ -17,7 +16,9 @@ public class ClearSessionAction extends BaseAction {
      * 
      */
     private static final long serialVersionUID = 1L;
-    @Action(results={@Result(name=SUCCESS, location="index-success.jsp")})
+
+    @Action(results = { @Result(name = SUCCESS, type = "chain", params = {
+            "namespace", "/", "actionName", "index" }) })
     public String execute() {
         session.clear();
         return SUCCESS;
