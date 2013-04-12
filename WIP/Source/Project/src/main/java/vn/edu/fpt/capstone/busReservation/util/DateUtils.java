194d9df6bf9dd215705d5ab19a7d51c34b4ddedd
hainl60335 <hainl60335@fpt.edu.vn>
2013-04-12 14:31:52 +0000

    fix time zone

--- WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/DateUtils.java
+++ WIP/Source/Project/src/main/java/vn/edu/fpt/capstone/busReservation/util/DateUtils.java
@@ -44,13 +44,14 @@ public class DateUtils {
      * @return the formatted time string
      * @throws ParseException
      */
-    public static String date2String(final Date date, String pattern, Locale locale,
-            TimeZone timeZone) {
-    	long offset = 0;
+    public static String date2String(final Date date, String pattern,
+            Locale locale, TimeZone timeZone) {
+        long offset = 0;
         SimpleDateFormat formatter = new SimpleDateFormat(pattern, locale);
-//        formatter.setTimeZone(timeZone);
+        // formatter.setTimeZone(timeZone);
         offset = timeZone.getOffset(date.getTime());
-        return formatter.format(new Date(date.getTime() + offset));
+        return formatter.format(new Date(date.getTime()
+                - TimeZone.getDefault().getOffset(date.getTime()) + offset));
     }
 
     /**
@@ -75,47 +76,49 @@ public class DateUtils {
                 - timeZone.getOffset(milisec));
         return result;
     }
-    
-	/**
-	 * Add days in string format
-	 * 
-	 * @param inputDate
-	 * 			The date that needs to add
-	 * @param amount
-	 *  		The amount of days to be added
-	 * @param pattern
-	 * 			The pattern describing date format (ex: dd-MM-yyyy, yyyy/MM/dd,...)
-	 * @param locale
-	 * 			The locale whose date format symbol should be used
-	 * @return
-	 * 			The day that has been added in string (formatted in input pattern)
-	 */
-	public static String addDay(String inputDate, int amount, String pattern, Locale locale) 
-			throws ParseException {
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat formatter = new SimpleDateFormat(pattern, locale);
-		cal.setTime(formatter.parse(inputDate));
-		cal.add(Calendar.DAY_OF_MONTH, amount);
-		return formatter.format(cal.getTime());
-	}
-	
-	public static boolean isDateEqual(final Date date1, final Date date2) {
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd", Locale.US);
-		if(formatter.format(date1) == formatter.format(date2)){
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Get MM-dd String from Date
-	 * @param inputDate 
-	 * 			the date to get String 
-	 * @return
-	 * 			MM-dd String
-	 */
-	public static String getMonthDay(final Date inputDate) {
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM", Locale.US);
-		return formatter.format(inputDate);
-	}
+
+    /**
+     * Add days in string format
+     * 
+     * @param inputDate
+     *            The date that needs to add
+     * @param amount
+     *            The amount of days to be added
+     * @param pattern
+     *            The pattern describing date format (ex: dd-MM-yyyy,
+     *            yyyy/MM/dd,...)
+     * @param locale
+     *            The locale whose date format symbol should be used
+     * @return The day that has been added in string (formatted in input
+     *         pattern)
+     */
+    public static String addDay(String inputDate, int amount, String pattern,
+            Locale locale) throws ParseException {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern, locale);
+        cal.setTime(formatter.parse(inputDate));
+        cal.add(Calendar.DAY_OF_MONTH, amount);
+        return formatter.format(cal.getTime());
+    }
+
+    public static boolean isDateEqual(final Date date1, final Date date2) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd",
+                Locale.US);
+        if (formatter.format(date1) == formatter.format(date2)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get MM-dd String from Date
+     * 
+     * @param inputDate
+     *            the date to get String
+     * @return MM-dd String
+     */
+    public static String getMonthDay(final Date inputDate) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM", Locale.US);
+        return formatter.format(inputDate);
+    }
 }
